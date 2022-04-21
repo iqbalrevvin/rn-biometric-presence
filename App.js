ef2cb351f6e8cc56812ce0dcdff0d3da0b3a7fc7
Iqbal Revvin <48580953+iqbalrevvin@users.noreply.github.com>
2022-04-21 20:26:26 +0700

    feat(boilerplate): Define splash screen & logo

--- App.js
+++ App.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-lines-per-function */
 import React from 'react';
 import { ThemeProvider } from 'react-native-elements';
 import { QueryClient, QueryClientProvider } from 'react-query';
@@ -7,20 +8,28 @@ import Toast from 'react-native-toast-message';
 import Routes from './src/Routes';
 import { Persistor, Store } from './src/Services/Redux/store';
 import { toastConfig } from '~Components/Toast/toast.config';
+import SplashScreen from 'react-native-splash-screen';
 
 const queryClient = new QueryClient();
 
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <ThemeProvider>
-      <Provider store={Store}>
-        <PersistGate loading={null} persistor={Persistor}>
-          <Routes />
-          <Toast config={toastConfig} />
-        </PersistGate>
-      </Provider>
-    </ThemeProvider>
-  </QueryClientProvider>
-);
+const App = () => {
+
+  React.useEffect(() => {
+    SplashScreen.hide();
+  });
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider>
+        <Provider store={Store}>
+          <PersistGate loading={null} persistor={Persistor}>
+            <Routes />
+            <Toast config={toastConfig} />
+          </PersistGate>
+        </Provider>
+      </ThemeProvider>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
