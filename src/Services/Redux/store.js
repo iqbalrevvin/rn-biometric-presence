c91df9c91b1ef303b598c554a56f18ef3b4a6498
Iqbal Revvin <48580953+iqbalrevvin@users.noreply.github.com>
2022-04-24 23:10:58 +0700

    feat(login): Setup persisting login

--- src/Services/Redux/store.js
+++ src/Services/Redux/store.js
@@ -7,7 +7,7 @@ const persistConfig = {
     key: 'root',
     storage: AsyncStorage,
     blacklist: ['loading'],
-    whitelist: [''],
+    whitelist: ['auth'],
 };
 
 const persistedReducer = persistReducer(persistConfig, Reducer);
